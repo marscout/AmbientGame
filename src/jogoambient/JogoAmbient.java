ba9efeed3faeb218d661aa47c6342f650c026ca4
Marcelo Rondini <marcelo-salvioni@hotmail.com>
2019-10-19 19:31:21 -0300

    Finalizado as principais funçoes

--- src/jogoambient/JogoAmbient.java
+++ src/jogoambient/JogoAmbient.java
@@ -5,6 +5,8 @@
  */
 package jogoambient;
 
+import java.io.File;
+import java.io.FileInputStream;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -13,38 +15,46 @@ import javafx.geometry.Orientation;
 import javafx.scene.Scene.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
-
 
 /**
  *
  * @author marce
  */
 public class JogoAmbient extends Application {
+//    
+//    private ImageView imageView;
 
-    private Button btnMetal = new Button("Metal");
-    private Button btnOrganico = new Button("Organico");
-    private Button btnPlastico = new Button("Plastico");
-    private Button btnVidro = new Button("Vidro");
-    private Button btnPapel = new Button("Papel");
+    private Button btnMetal = new Button();
+    private Button btnOrganico = new Button();
+    private Button btnPlastico = new Button();
+    private Button btnVidro = new Button();
+    private Button btnPapel = new Button();
     public Label lVisor = new Label("2");
     Label lPontos = new Label("0");
     private Label lPontosTxt = new Label("Pontos: ");
 
+    Pane pVisor = new Pane();
+
     @Override
     public void start(Stage stage) {
-        //ImageView ivMateriais = new ImageView(getClass().getResource(""));
+
+        pVisor.setPrefSize(500, 500);
+    
+        AlterarBotoes();
+        alterarVisor();
+
         Pane pPontos = new Pane(lPontos);
         HBox hbPontos = new HBox(10, lPontosTxt, pPontos);
 
-        Pane pVisor = new Pane(lVisor);
         TilePane tpBotoes = new TilePane(Orientation.HORIZONTAL, btnMetal, btnOrganico, btnPapel, btnPlastico, btnVidro);
         BorderPane bpVisor = new BorderPane(pVisor, hbPontos, null, tpBotoes, null);
         alterarVisor();
 
-        Scene scene = new Scene(bpVisor, 350, 350);
+        Scene scene = new Scene(bpVisor);
 
         stage.setTitle("Hello World!");
         stage.setScene(scene);
@@ -55,14 +65,10 @@ public class JogoAmbient extends Application {
             String spontos = lPontos.getText();
             if (valor.equals("0")) {
 
-               // pPontos.getChildren().clear();
-
                 int iPontos = Integer.parseInt(spontos);
                 iPontos += 1;
-                //Label lPontos = new Label(String.valueOf(iPontos));
 
-                //pPontos.getChildren().add(lPontos);
-                lPontos.setText(""+String.valueOf(iPontos));
+                lPontos.setText("" + String.valueOf(iPontos));
 
                 alterarVisor();
             } else {
@@ -81,14 +87,11 @@ public class JogoAmbient extends Application {
             String pontos = lPontos.getText();
 
             if (valor.equals("1")) {
-                //pPontos.getChildren().clear();
 
                 int iPontos = Integer.parseInt(pontos);
                 iPontos += 1;
-                //Label lPontos = new Label(String.valueOf(iPontos));
 
-                //pPontos.getChildren().add(lPontos);
-                lPontos.setText(""+String.valueOf(iPontos));
+                lPontos.setText("" + String.valueOf(iPontos));
                 alterarVisor();
             } else {
                 int numero = Integer.parseInt(pontos);
@@ -106,14 +109,11 @@ public class JogoAmbient extends Application {
 
             String pontos = lPontos.getText();
             if (valor.equals("2")) {
-                //pPontos.getChildren().clear();
 
                 int iPontos = Integer.parseInt(pontos);
                 iPontos += 1;
-                //Label lPontos = new Label(String.valueOf(iPontos));
 
-                //pPontos.getChildren().add(lPontos);
-                lPontos.setText(""+String.valueOf(iPontos));
+                lPontos.setText("" + String.valueOf(iPontos));
                 alterarVisor();
             } else {
                 int numero = Integer.parseInt(pontos);
@@ -130,14 +130,11 @@ public class JogoAmbient extends Application {
             String valor = lVisor.getText();
             String pontos = lPontos.getText();
             if (valor.equals("3")) {
-                //pPontos.getChildren().clear();
 
                 int iPontos = Integer.parseInt(pontos);
                 iPontos += 1;
-                //Label lPontos = new Label(String.valueOf(iPontos));
 
-                //pPontos.getChildren().add(lPontos);
-                lPontos.setText(""+String.valueOf(iPontos));
+                lPontos.setText("" + String.valueOf(iPontos));
                 alterarVisor();
             } else {
                 int numero = Integer.parseInt(pontos);
@@ -154,14 +151,11 @@ public class JogoAmbient extends Application {
             String valor = lVisor.getText();
             String pontos = lPontos.getText();
             if (valor.equals("4")) {
-                //.getChildren().clear();
 
                 int iPontos = Integer.parseInt(pontos);
                 iPontos += 1;
-                //Label lPontos = new Label(String.valueOf(iPontos));
 
-                //pPontos.getChildren().add(lPontos);
-                lPontos.setText(""+String.valueOf(iPontos));
+                lPontos.setText("" + String.valueOf(iPontos));
                 alterarVisor();
             } else {
                 int numero = Integer.parseInt(pontos);
@@ -176,38 +170,80 @@ public class JogoAmbient extends Application {
         });
     }
 
-    /* public void verificarAcerto(ActionEvent e) {
-        String valor = lVisor.getText();
-        String pontos = lPontos.getText();
-        e.getSource();
-        if (btnMetal.getText() == "Metal" && valor == "0") {
-            int numero = Integer.parseInt(pontos);
-            lPontos.setText(" " + (numero += 1));
-            alterarVisor();
-        } else if (btnOrganico.getText() == "Organico" && valor == "1") {
-            int numero = Integer.parseInt(pontos);
-            lPontos.setText(" " + (numero += 1));
-            alterarVisor();
-        } else if (btnPapel.getText() == "Papel" && valor == "2") {
-            int numero = Integer.parseInt(pontos);
-            lPontos.setText(" " + (numero += 1));
-            alterarVisor();
-        } else if (btnPlastico.getText() == "Plastico" && valor == "3") {
-            int numero = Integer.parseInt(pontos);
-            lPontos.setText(" " + (numero += 1));
-            alterarVisor();
-        } else if (btnVidro.getText() == "Vidro" && valor == "4") {
-            int numero = Integer.parseInt(pontos);
-            lPontos.setText(" " + (numero += 1));
-            alterarVisor();
+    public void alterarVisor() {
+        int sNumero = (int) Math.round(Math.random() * 4);
+        Image image;
+        ImageView imageView = new ImageView();
+        imageView.setFitHeight(350);
+        imageView.setFitWidth(350);
+        imageView.setLayoutX(125);
+        imageView.setLayoutY(125);
+
+        if (sNumero < 1) {
+            image = new Image("lixoMetal.jpg");
+            imageView.setImage(image);
+            pVisor.getChildren().add(imageView);
+            lVisor.setText(String.valueOf(sNumero));
+        } else if (sNumero < 2) {
+            image = new Image("lixoOrganico.jpg");
+            imageView.setImage(image);
+            pVisor.getChildren().add(imageView);
+            lVisor.setText(String.valueOf(sNumero));
+        } else if (sNumero < 3) {
+            image = new Image("lixoPapel.jpg");
+            imageView.setImage(image);
+            pVisor.getChildren().add(imageView);
+            lVisor.setText(String.valueOf(sNumero));
+        } else if (sNumero < 4) {
+            image = new Image("lixoPlastico.jpg");
+            imageView.setImage(image);
+            pVisor.getChildren().add(imageView);
+            lVisor.setText(String.valueOf(sNumero));
+        } else {
+            image = new Image("lixoVidro.jpg");
+            imageView.setImage(image);
+            pVisor.getChildren().add(imageView);
+            lVisor.setText(String.valueOf(sNumero));
         }
 
     }
-     */
-    public void alterarVisor() {
-        int sNumero = (int) Math.round(Math.random() * 4);
 
-        lVisor.setText(String.valueOf(sNumero));
+    public void AlterarBotoes() {
+        Image imageMetal = new Image("botaoMetal.png");
+        Image imageOrganico = new Image("botaoOrganico.jpg");
+        Image imagePapel = new Image("botaoPapel1.jpg");
+        Image imagePlastico = new Image("botaoPlastico.png");
+        Image imageVidro = new Image("botaoVidro.jpg");
+        ImageView imageView = new ImageView(imageMetal);
+        imageView.setFitHeight(120);
+        imageView.setFitWidth(115);
+        
+        ImageView imageView2 = new ImageView(imageOrganico);
+        imageView2.setFitHeight(120);
+        imageView2.setFitWidth(115);
+        
+        ImageView imageView3 = new ImageView(imagePapel);
+        imageView3.setFitHeight(120);
+        imageView3.setFitWidth(115);
+        
+        ImageView imageView4 = new ImageView(imagePlastico);
+        imageView4.setFitHeight(120);
+        imageView4.setFitWidth(115);
+        
+        ImageView imageView5 = new ImageView(imageVidro);
+        imageView5.setFitHeight(120);
+        imageView5.setFitWidth(115);
+        
+        btnMetal.setGraphic(imageView);
+
+        btnOrganico.setGraphic(imageView2);
+        
+        btnPapel.setGraphic(imageView3);
+        
+        btnPlastico.setGraphic(imageView4);
+        
+        btnVidro.setGraphic(imageView5);
+
     }
 
     public static void main(String[] args) {
